a2fb87c1d193baea7d1eb59be86a4ecc63431eb7
Amol <amolgawade56@gmail.com>
2024-09-08 19:09:45 +0530

    Config file handling middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+
+    
+  const uploadOnCloudinary = async (localfilepath) => {
+          
+    try {
+        if(!localfilepath) return null;
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto"
+        })
+        //File has been uploaded successfull 
+        console.log("File is Uploaded");
+        console.log(response.url);
+        return response;
+        
+        
+    } catch (error) {
+        fs.unlinkSync(localfilepath)
+        //remove the locally save temporary file as the operation get failed 
+        return null;
+    }
+  }
+
+
+  export {uploadOnCloudinary }
