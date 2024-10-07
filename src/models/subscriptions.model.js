f32326d3b057420f44a741fed746fc08c5ce757c
Amol <amolgawade56@gmail.com>
2024-10-07 23:28:02 +0530

    Add Aggregate Pipelines && add user routes

--- src/models/subscriptions.model.js
+++ src/models/subscriptions.model.js
@@ -1,5 +1,12 @@
 import mongoose,{Schema} from "mongoose"
 
+//How to count subscribers of a channel?
+//-> 1) Take the channel name and search in schema through channel  
+//   2) In return we get many documents through channel count them 
+
+// How to know which channel a user subscribe ?
+//=>  1)we take user id and search in schema through subscriber 
+//    2)In return document we check the channel name and give the list 
 
 const SubscriptionSchema = new Schema({
     subscriber:{
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { currentUser, getUserChannelInfo, getUserWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -17,5 +17,12 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,currentUser);
+router.route("/update-user").patch(verifyJWT,updateUserDetails);
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
+router.route("/channel/:username").get(verifyJWT,getUserChannelInfo);
+router.route("/watch-history").get(verifyJWT,getUserWatchHistory);
 export default router;
